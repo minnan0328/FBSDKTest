45a33272828fbd1c262079321e0964febba2756c
minnan0328 <minnan0328@gmail.com>
2019-11-13 20:40:30 +0800

    測試

--- public/DeviceJudgment.js
+++ public/DeviceJudgment.js
@@ -28,5 +28,5 @@
     })
     console.log(WhatSystem)
     console.log(MobileType)
-    gameInstance.SendMessage("Root", "FromHtml_str", `${MobileType}`)
+    // gameInstance.SendMessage("Root", "FromHtml_str", `${MobileType}`)
 })()
